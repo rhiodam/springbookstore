46dd05a0ad6e1324449c19ea30d8ef808604e6a3
rhiodam <rhioda.m@gmail.com>
2019-03-04 07:52:49 +0700

    Initial Project

---
+++ src/main/java/com/rhiodamuthie/eye/springbookstore/SpringbookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.rhiodamuthie.eye.springbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rhiodamuthie/eye/springbookstore/SpringbookstoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rhiodamuthie.eye.springbookstore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbookstoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
